c37c817437d3e1b29d3fd5fa6c2a144d0d9bd4f9
Frank Rietta <frank@rietta.com>
2018-12-30 21:46:18 -0500

    The test suite now runs!

--- tests/board_test.cpp
+++ tests/board_test.cpp
@@ -1,5 +1,7 @@
-#define BOOST_TEST_MODULE const_string Minesweeper Board
+#define BOOST_TEST_MODULE const_string test
 #include <boost/test/unit_test.hpp>
 
-#include "../models/board.h"
-
+BOOST_AUTO_TEST_CASE(dummy)
+{
+  BOOST_CHECK(1 + 1 == 2);
+}
